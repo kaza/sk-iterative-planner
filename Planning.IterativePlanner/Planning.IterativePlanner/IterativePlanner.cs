43e9e0f0c2220ce7141333e5b140e19707d00ce8
Almir Kazazic <kazazic@gmail.com>
2023-05-12 12:06:54 +0200

    initial commit

---
+++ Planning.IterativePlanner/Planning.IterativePlanner/IterativePlanner.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Diagnostics;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Planning;
+using Microsoft.SemanticKernel.Planning.Sequential;
+using Microsoft.SemanticKernel.SkillDefinition;
+using Planning.IterativePlanner;
+
+// namespace Planning.IterativePlanner;
+namespace Microsoft.SemanticKernel.Planning;
+
+/// <summary>
+/// A planner that uses semantic function to create a sequential plan.
+/// </summary>
+public sealed class IterativePlanner
+{
+    private const string StopSequence = "<!--";
+
+    /// <summary>
+    /// Initialize a new instance of the <see cref="IterativePlanner"/> class.
+    /// </summary>
+    /// <param name="kernel">The semantic kernel instance.</param>
+    /// <param name="config">The planner configuration.</param>
+    /// <param name="prompt">Optional prompt override</param>
+    public IterativePlanner(
+        IKernel kernel,
+        IterativePlannerConfig? config = null,
+        string? prompt = null)
+    {
+        Verify.NotNull(kernel);
+        this.Config = config ?? new();
+
+        this.Config.ExcludedSkills.Add(RestrictedSkillName);
+
+        string promptTemplate = prompt ?? EmbeddedResource.Read("skprompt.txt");
+
+        this._functionFlowFunction = kernel.CreateSemanticFunction(
+            promptTemplate: promptTemplate,
+            skillName: RestrictedSkillName,
+            description: "Given a request or command or goal generate a step by step plan to " +
+                         "fulfill the request using functions. This ability is also known as decision making and function flow",
+            maxTokens: this.Config.MaxTokens,
+            temperature: 0.0,
+            stopSequences: new[] { StopSequence });
+
+        this._context = kernel.CreateNewContext();
+    }
+
+    /// <summary>
+    /// Create a plan for a goal.
+    /// </summary>
+    /// <param name="goal">The goal to create a plan for.</param>
+    /// <returns>The plan.</returns>
+    public async Task<Plan> CreatePlanAsync(string goal)
+    {
+        if (string.IsNullOrEmpty(goal))
+        {
+            throw new PlanningException(PlanningException.ErrorCodes.InvalidGoal, "The goal specified is empty");
+        }
+
+        string relevantFunctionsManual = await this._context.GetFunctionsManualAsync(goal, this.Config).ConfigureAwait(false);
+        this._context.Variables.Set("available_functions", relevantFunctionsManual);
+
+        this._context.Variables.Update(goal);
+
+        var planResult = await this._functionFlowFunction.InvokeAsync(this._context).ConfigureAwait(false);
+
+        string planResultString = planResult.Result.Trim();
+
+        try
+        {
+            var plan = planResultString.ToPlanFromXml(goal, this._context);
+            return plan;
+        }
+        catch (Exception e)
+        {
+            throw new PlanningException(PlanningException.ErrorCodes.InvalidPlan, "Plan parsing error, invalid XML", e);
+        }
+    }
+
+    private IterativePlannerConfig Config { get; }
+
+    private readonly SKContext _context;
+
+    /// <summary>
+    /// the function flow semantic function, which takes a goal and creates an xml plan that can be executed
+    /// </summary>
+    private readonly ISKFunction _functionFlowFunction;
+
+    /// <summary>
+    /// The name to use when creating semantic functions that are restricted from plan creation
+    /// </summary>
+    private const string RestrictedSkillName = "SequentialPlanner_Excluded";
+}
